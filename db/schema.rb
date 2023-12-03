bca8947cf64ce71b70f759f59beffbea14958b9e
unknown <abi.kh03@gmail.com>
2023-12-04 08:47:38 +1100

    new Image polymorfic association established and seeds is loaded with some data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_213159) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_03_213823) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "email"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,48 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# seeds.rb
+
+# Create authors
+authors = []
+10.times do |i|
+  authors << Author.create!(
+    name: "Author #{i + 1}",
+    email: "author#{i + 1}@example.com"
+  )
+end
+
+# Create categories
+categories = []
+10.times do |i|
+  categories << Category.create!(
+    name: "Category #{i + 1}"
+  )
+end
+
+# Create books with authors and categories
+10.times do |i|
+  author = authors.sample
+  categories_for_book = categories.sample(2)  # Assuming each book has two categories
+
+  book = Book.create!(
+    title: "Book #{i + 1}",
+    isbn: "123-#{i + 1000000000}",
+    author: author
+  )
+
+  # Associate categories with the book through Categorization
+  categories_for_book.each do |category|
+    Categorization.create!(
+      book: book,
+      category: category
+    )
+  end
+
+  # Create cover image for each book with a placeholder image URL
+  book.create_cover_image(
+    url: "https://picsum.photos/200/300?random=#{i + 1}"  # Placeholder image URL from Lorem Picsum
+  )
+end
